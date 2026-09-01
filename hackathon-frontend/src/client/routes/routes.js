5eef3102c07d291a439a1c6d7dace2a6ea72fde0
cong.trung.nguyen <cong.trung.nguyen@intel.com>
2026-09-01 13:23:07 +0700

    style: format CSS and JavaScript for improved readability

--- hackathon-frontend/src/client/routes/routes.js
+++ hackathon-frontend/src/client/routes/routes.js
@@ -107,14 +107,14 @@ export const routes = [
         ],
     },
     {
-    route_name: routesConfig.pypcs_page,
-    key: 'pypcs_page',
-    title: 'PYPCS Matrix',
-    icon: TablePropertiesIcon,
-    breadcrumb: ['PYPCS Matrix'],
-    component: lazy(() =>
-        import('@/features/pypcs/components/PYPCSMatrixTab'),
-    ),
+        route_name: routesConfig.pypcs_page,
+        key: 'pypcs_page',
+        title: 'PYPCS Matrix',
+        icon: TablePropertiesIcon,
+        breadcrumb: ['PYPCS Matrix'],
+        component: lazy(
+            () => import('@/features/pypcs/components/PYPCSMatrixTab'),
+        ),
     },
     {
         route_name: routesConfig.chat_page,
